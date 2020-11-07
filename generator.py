17aa12d84bc83171e3965ea2a64ee0a29048f7fb
FrostyJet <edhovhannisyan97@gmail.com>
2020-11-07 18:40:17 +0400

    Initial

---
+++ generator.py
@@ -0,0 +1,117 @@
+import requests
+from lxml import html
+from PIL import Image
+
+searchEngine = "yandex"
+
+query = input("What are you looking for? \r\n")
+
+if searchEngine == "google":
+    searchQueryUrl = "https://www.google.com/search?q=%s&tbm=isch" % (query)
+elif searchEngine == "yandex":
+    searchQueryUrl = "https://yandex.com/images/search?from=tabbar&text=%s" % (query)
+elif searchEngine == "yahoo":
+    searchQueryUrl = "https://images.search.yahoo.com/search/images?p=%s" % (query)
+
+page = requests.get(searchQueryUrl)
+
+f = open("composition.source.html", "wb")
+f.write(page.content)
+f.close()
+
+tree = html.fromstring(page.content)
+
+if searchEngine == "google":
+    images = tree.xpath("//tr//img")
+elif searchEngine == "yandex":
+    images = tree.xpath("//img[contains(@class, 'serp-item__thumb')]")
+elif searchEngine == "yahoo":
+    images = tree.xpath("//*[@id='results']//img")
+
+cols = 5
+ideal_width = int(input("Width? \r\n"))
+ideal_height = int(input("Height? \r\n"))
+
+imgIds = []
+for i in range(0, cols):
+    row = []
+    for j in range(0, cols):
+        index = cols * i + j
+        if index >= len(images):
+            break
+
+        row.append(0)
+        img = images[index]
+
+        if searchEngine == "google" or searchEngine == 'yahoo':
+            imgSrc = img.get("src")
+        elif searchEngine == "yandex":
+            imgSrc = 'https:' + img.get("src")
+
+        print(imgSrc)
+        if not imgSrc: continue
+
+        result = requests.get(imgSrc)
+
+        file = open("sources/" + str(index) + ".png", "wb")
+        file.write(result.content)
+        file.close()
+        pass
+    imgIds.append(row)
+    pass
+
+blankImage = Image.new(
+    "RGBA", (cols * ideal_width, cols * ideal_height), "BLACK")
+
+for i in range(0, cols):
+    for j in range(0, cols):
+        index = cols * i + j
+        if index >= len(images):
+            break
+
+        gridSection = Image.open("sources/" + str(index) + ".png")
+
+        x = 0
+        y = 0
+
+        if i > 0:
+            x = imgIds[i - 1][j]["x"] + imgIds[i - 1][j]["w"]
+
+        if j > 0:
+            y = imgIds[i][j - 1]["y"] + imgIds[i][j - 1]["h"]
+
+        aspect = gridSection.width / float(gridSection.height)
+
+        ideal_aspect = ideal_width / float(ideal_height)
+
+        if aspect > ideal_aspect:
+            # Then crop the left and right edges:
+            new_width = int(ideal_aspect * gridSection.height)
+            offset = (gridSection.width - new_width) / 2
+            resize = (offset, 0, gridSection.width -
+                      offset, gridSection.height)
+        else:
+            # ... crop the top and bottom:
+            new_height = int(gridSection.width / ideal_aspect)
+            offset = (gridSection.height - new_height) / 2
+            resize = (0, offset, gridSection.width,
+                      gridSection.height - offset)
+
+        thumb = gridSection.crop(resize).resize(
+            (ideal_width, ideal_height), Image.ANTIALIAS)
+
+        imgIds[i][j] = {
+            "x": x,
+            "y": y,
+            "w": ideal_width,
+            "h": ideal_height
+        }
+
+        print(imgIds[i][j])
+
+        blankImage.paste(thumb, (x, y))
+        pass
+    pass
+
+blankImage.save("composition.png", quality=95)
+blankImage.show()
